14e89d0f08722f7d0c0645a8b714d2b487848b9a
Joshua Clingenpeel <joshua.clingenpeel@gmail.com>
2009-03-25 23:22:07 -0700

    adding the comments scaffolding.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :comments
+
   map.resources :pictures
 
   # The priority is based upon order of creation: first created -> highest priority.
