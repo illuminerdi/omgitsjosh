39971085ea4169c0f60d0ba48ee392b9a19b0e3f
Joshua Clingenpeel <joshua.clingenpeel@gmail.com>
2009-03-25 19:17:54 -0700

    initial commit, rails 2.2.2 app and pictures scaffolding in place.

---
+++ db/migrate/20090326021644_create_pictures.rb
@@ -0,0 +1,15 @@
+class CreatePictures < ActiveRecord::Migration
+  def self.up
+    create_table :pictures do |t|
+      t.string :name
+      t.string :content_type
+      t.binary :data
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :pictures
+  end
+end
