dbb47f9ff87ae9936ddbb00334044960d63fb6e6
Joshua Clingenpeel <joshua.clingenpeel@gmail.com>
2009-03-27 18:04:25 -0700

    changing environment and app to work with sessions.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => '540ca6df6df7e32e93fb111d8a090171'
+  protect_from_forgery :secret => '540ca6df6df7e32e93fb111d8a090171'
   
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
